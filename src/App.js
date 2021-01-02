1a90a6e00416e89ab22a064fac24312bf8d09eea
Kyle Pykonen <kylepyk@gmail.com>
2021-01-01 20:34:45 -0800

    Syntax fixes, quality assurance

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { useEffect, useState} from "react";
-import Filterer from "./Filterer";
-import table from "./table"
+import React, { useEffect, useState } from "react";
+import FilterInput from "./FilterInput";
+import Table from "./Table";
 import { getUsers } from "./API";
-import './App.css';
-import logo from './logo.svg';
+import "./App.css";
 
 function App() {
-  const  [initialUsers, updateAvailableUsers] = useState([]);
-  const  [usersToRender, updateUsersToRender] = useState([]);
+  const [initialUsers, updateAvailableUsers] = useState([]);
+  const [usersToRender, updateUsersToRender] = useState([]);
+
   useEffect(() => {
     getUsers().then(({ data: { results } }) => updateAvailableUsers(results));
   }, []);
+
   return (
-    <div classname="App">
+    <div className="App">
       <h1>Employee Directory</h1>
-      <p>PLACEHOLDER TEXT ABOUT FILTERING</p>
-      <filter users={initialUsers} updateUsers={updateUsersToRender} />
-      <table users={usersToRender} />
+      <p>
+        To sort employees by first name, begin entering a first name below
+      </p>
+      <FilterInput users={initialUsers} updateUsers={updateUsersToRender} />
+      <Table users={usersToRender} />
     </div>
   );
-
 }
+
+//test
+
 // function App() {
 //   return (
 //     <div className="App">
